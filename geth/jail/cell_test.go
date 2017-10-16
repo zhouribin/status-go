2257aeec8b5edcfbc0bdf39faa5ed536439ca180
Ivan Danyliuk <ivan.daniluk@gmail.com>
2017-10-16 16:24:29 +0300

    Move jail cell tests to GoConvey

--- geth/jail/cell_test.go
+++ geth/jail/cell_test.go
@@ -3,340 +3,229 @@ package jail_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"testing"
 	"time"
 
 	"github.com/robertkrimen/otto"
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/status-im/status-go/geth/jail"
 	"github.com/status-im/status-go/static"
-	"github.com/stretchr/testify/suite"
 )
 
-const (
-	testChatID = "testChat"
-)
+const testChatID = "testChat"
 
-var (
-	baseStatusJSCode = string(static.MustAsset("testdata/jail/status.js"))
-)
+var baseStatusJSCode = string(static.MustAsset("testdata/jail/status.js"))
 
-type CellTestSuite struct {
-	suite.Suite
-	jail *jail.Jail
-}
+func TestJailTimeout(t *testing.T) {
+	Convey("Jail should handle setTimeout correctly", t, func() {
+		cell, err := jail.New(nil).NewCell(testChatID)
+		So(err, ShouldBeNil)
+		So(cell, ShouldNotBeNil)
+		Reset(func() {
+			cell.Stop()
+		})
 
-func (s *CellTestSuite) SetupTest() {
-	s.jail = jail.New(nil)
-	s.NotNil(s.jail)
-}
+		Convey("setTimeout single function", func() {
+			_, err = cell.Run(`
+			var timerCounts = 0;
+			setTimeout(function(n){
+				if (Date.now() - n < 50) {
+					throw new Error("Timed out");
+				}
 
-func (s *CellTestSuite) TestJailTimeout() {
-	require := s.Require()
+				timerCounts++;
+			}, 50, Date.now());
+			`)
+			So(err, ShouldBeNil)
 
-	cell, err := s.jail.NewCell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-	defer cell.Stop()
+			// wait at least 10x longer to decrease probability
+			// of false negatives as we using real clock here
+			time.Sleep(300 * time.Millisecond)
 
-	// Attempt to run a timeout string against a Cell.
-	_, err = cell.Run(`
-		var timerCounts = 0;
- 		setTimeout(function(n){
- 			if (Date.now() - n < 50) {
- 				throw new Error("Timed out");
- 			}
+			value, err := cell.Get("timerCounts")
+			So(err, ShouldBeNil)
+			So(value.IsNumber(), ShouldBeTrue)
+			So(value.String(), ShouldEqual, "1")
+		})
+		Convey("multiple setTimeout shouldn't race", func() {
+			items := make(chan struct{})
 
-			timerCounts++;
- 		}, 50, Date.now());
- 	`)
-	require.NoError(err)
+			err = cell.Set("__captureResponse", func() otto.Value {
+				go func() { items <- struct{}{} }()
+				return otto.UndefinedValue()
+			})
+			So(err, ShouldBeNil)
 
-	// wait at least 10x longer to decrease probability
-	// of false negatives as we using real clock here
-	time.Sleep(300 * time.Millisecond)
+			_, err = cell.Run(`
+			function callTimeoutFunc(){
+				return setTimeout(function(){
+					__captureResponse();
+				}, 1000);
+			}
+			`)
+			So(err, ShouldBeNil)
 
-	value, err := cell.Get("timerCounts")
-	require.NoError(err)
-	require.True(value.IsNumber())
-	require.Equal("1", value.String())
-}
+			N := 25
+			for i := 0; i < N; i++ {
+				_, err = cell.Call("callTimeoutFunc", nil)
+				So(err, ShouldBeNil)
+			}
+			for i := 0; i < N; i++ {
+				select {
+				case <-items:
+				case <-time.After(5 * time.Second):
+					t.Fatal("test timed out")
+				}
+			}
+		})
+		Convey("setTimeout should be cancelled properly", func() {
+			var count int
+			err = cell.Set("__captureResponse", func(val string) otto.Value {
+				count++
+				return otto.UndefinedValue()
+			})
+			So(err, ShouldBeNil)
 
-func (s *CellTestSuite) TestJailLoopInCall() {
-	require := s.Require()
+			_, err = cell.Run(`
+			function callTimeoutFunc(val, delay){
+				return setTimeout(function(){
+					__captureResponse(val);
+				}, delay);
+			}
+			`)
+			So(err, ShouldBeNil)
 
-	// load Status JS and add test command to it
-	s.jail.BaseJS(baseStatusJSCode)
-	s.jail.Parse(testChatID, ``)
+			// Run 5 timeout tasks to be executed in: 1, 2, 3, 4 and 5 secs
+			for i := 1; i <= 5; i++ {
+				_, err = cell.Call("callTimeoutFunc", nil, "value", i*100)
+				So(err, ShouldBeNil)
+			}
 
-	cell, err := s.jail.Cell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-	defer cell.Stop()
+			// Wait 1.5 second (so only one task executed) so far
+			// and stop the cell (event loop should die)
+			time.Sleep(150 * time.Millisecond)
+			cell.Stop()
 
-	items := make(chan string)
+			// check that only 1 task has increased counter
+			So(count, ShouldEqual, 1)
 
-	err = cell.Set("__captureResponse", func(val string) otto.Value {
-		go func() { items <- val }()
-		return otto.UndefinedValue()
+			// wait 2 seconds more (so at least two more tasks would
+			// have been executed if event loop is still running)
+			<-time.After(200 * time.Millisecond)
+
+			// check that counter hasn't increased
+			So(count, ShouldEqual, 1)
+		})
 	})
-	require.NoError(err)
-
-	_, err = cell.Run(`
-		function callRunner(namespace){
-			console.log("Initiating callRunner for: ", namespace)
-			return setTimeout(function(){
-				__captureResponse(namespace);
-			}, 1000);
-		}
-	`)
-	require.NoError(err)
-
-	_, err = cell.Call("callRunner", nil, "softball")
-	require.NoError(err)
-
-	select {
-	case received := <-items:
-		require.Equal(received, "softball")
-	case <-time.After(5 * time.Second):
-		require.Fail("Failed to received event response")
-	}
 }
 
-// TestJailLoopRace tests multiple setTimeout callbacks,
-// supposed to be run with '-race' flag.
-func (s *CellTestSuite) TestJailLoopRace() {
-	require := s.Require()
+func TestJailFetch(t *testing.T) {
+	Convey("Jail should handle FetchAPI correctly", t, func() {
+		cell, err := jail.New(nil).NewCell(testChatID)
+		So(err, ShouldBeNil)
+		So(cell, ShouldNotBeNil)
 
-	cell, err := s.jail.NewCell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-	defer cell.Stop()
+		body := `{"key": "value"}`
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Content-Type", "application/json")
+			w.Write([]byte(body))
+		}))
 
-	items := make(chan struct{})
+		Reset(func() {
+			cell.Stop()
+			server.Close()
+		})
 
-	err = cell.Set("__captureResponse", func() otto.Value {
-		go func() { items <- struct{}{} }()
-		return otto.UndefinedValue()
+		dataCh := make(chan otto.Value, 1)
+		errCh := make(chan otto.Value, 1)
+
+		err = cell.Set("__captureSuccess", func(res otto.Value) { dataCh <- res })
+		So(err, ShouldBeNil)
+		err = cell.Set("__captureError", func(res otto.Value) { errCh <- res })
+		So(err, ShouldBeNil)
+
+		Convey("Fetching valid URL should work", func() {
+			_, err = cell.Run(`fetch('` + server.URL + `').then(function(r) {
+				return r.text()
+			}).then(function(data) {
+				__captureSuccess(data)
+			}).catch(function (e) {
+				__captureError(e)
+			})`)
+			So(err, ShouldBeNil)
+
+			select {
+			case data := <-dataCh:
+				So(data.IsString(), ShouldBeTrue)
+				So(data.String(), ShouldEqual, body)
+			case err := <-errCh:
+				t.Fatalf("request failed: %v", err)
+			case <-time.After(1 * time.Second):
+				t.Fatal("test timed out")
+			}
+		})
+		Convey("Fetching invalid URL should provoke error", func() {
+			_, err = cell.Run(`fetch('http://👽/nonexistent').then(function(r) {
+				return r.text()
+			}).then(function(data) {
+				__captureSuccess(data)
+			}).catch(function (e) {
+				__captureError(e)
+			})`)
+			So(err, ShouldBeNil)
+
+			select {
+			case data := <-dataCh:
+				t.Fatalf("request should have failed, but returned: %v", data)
+			case e := <-errCh:
+				So(e.IsObject(), ShouldBeTrue)
+				name, err := e.Object().Get("name")
+				So(err, ShouldBeNil)
+				So(name.String(), ShouldEqual, "Error")
+				msg, err := e.Object().Get("message")
+				So(err, ShouldBeNil)
+				So(msg.String(), ShouldNotBeEmpty)
+			case <-time.After(1 * time.Second):
+				t.Fatal("test timed out")
+			}
+		})
+		Convey("Fetching concurrently shouldn't race", func() {
+			_, err := cell.Run(`fetch('` + server.URL + `').then(function(r) {
+				return r.text()
+			}).then(function(data) {
+				__captureSuccess(data)
+			}).catch(function (e) {
+				__captureError(e)
+			})`)
+			So(err, ShouldBeNil)
+
+			_, err = cell.Run(`fetch('http://👽/nonexistent').then(function(r) {
+				return r.text()
+			}).then(function(data) {
+				__captureSuccess(data)
+			}).catch(function (e) {
+				__captureError(e)
+			})`)
+			So(err, ShouldBeNil)
+
+			for i := 0; i < 2; i++ {
+				select {
+				case data := <-dataCh:
+					So(data.IsString(), ShouldBeTrue)
+					So(data.String(), ShouldEqual, body)
+				case e := <-errCh:
+					So(e.IsObject(), ShouldBeTrue)
+					name, err := e.Object().Get("name")
+					So(err, ShouldBeNil)
+					So(name.String(), ShouldEqual, "Error")
+					msg, err := e.Object().Get("message")
+					So(err, ShouldBeNil)
+					So(msg.String(), ShouldNotBeEmpty)
+				case <-time.After(1 * time.Second):
+					t.Fatal("test timed out")
+					return
+				}
+			}
+		})
 	})
-	require.NoError(err)
-
-	_, err = cell.Run(`
-		function callRunner(){
-			return setTimeout(function(){
-				__captureResponse();
-			}, 1000);
-		}
-	`)
-	require.NoError(err)
-
-	for i := 0; i < 100; i++ {
-		_, err = cell.Call("callRunner", nil)
-		require.NoError(err)
-	}
-
-	for i := 0; i < 100; i++ {
-		select {
-		case <-items:
-		case <-time.After(5 * time.Second):
-			require.Fail("test timed out")
-		}
-	}
-}
-
-func (s *CellTestSuite) TestJailFetchPromise() {
-	body := `{"key": "value"}`
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Write([]byte(body))
-	}))
-	defer server.Close()
-
-	require := s.Require()
-
-	cell, err := s.jail.NewCell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-	defer cell.Stop()
-
-	dataCh := make(chan otto.Value, 1)
-	errCh := make(chan otto.Value, 1)
-
-	err = cell.Set("__captureSuccess", func(res otto.Value) { dataCh <- res })
-	require.NoError(err)
-	err = cell.Set("__captureError", func(res otto.Value) { errCh <- res })
-	require.NoError(err)
-
-	// run JS code for fetching valid URL
-	_, err = cell.Run(`fetch('` + server.URL + `').then(function(r) {
-		return r.text()
-	}).then(function(data) {
-		__captureSuccess(data)
-	}).catch(function (e) {
-		__captureError(e)
-	})`)
-	require.NoError(err)
-
-	select {
-	case data := <-dataCh:
-		require.True(data.IsString())
-		require.Equal(body, data.String())
-	case err := <-errCh:
-		require.Fail("request failed", err)
-	case <-time.After(1 * time.Second):
-		require.Fail("test timed out")
-	}
-}
-
-func (s *CellTestSuite) TestJailFetchCatch() {
-	require := s.Require()
-
-	cell, err := s.jail.NewCell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-	defer cell.Stop()
-
-	dataCh := make(chan otto.Value, 1)
-	errCh := make(chan otto.Value, 1)
-
-	err = cell.Set("__captureSuccess", func(res otto.Value) { dataCh <- res })
-	require.NoError(err)
-	err = cell.Set("__captureError", func(res otto.Value) { errCh <- res })
-	require.NoError(err)
-
-	// run JS code for fetching invalid URL
-	_, err = cell.Run(`fetch('http://👽/nonexistent').then(function(r) {
-		return r.text()
-	}).then(function(data) {
-		__captureSuccess(data)
-	}).catch(function (e) {
-		__captureError(e)
-	})`)
-	require.NoError(err)
-
-	select {
-	case data := <-dataCh:
-		require.Fail("request should have failed, but returned", data)
-	case e := <-errCh:
-		require.True(e.IsObject())
-		name, err := e.Object().Get("name")
-		require.NoError(err)
-		require.Equal("Error", name.String())
-		_, err = e.Object().Get("message")
-		require.NoError(err)
-	case <-time.After(1 * time.Second):
-		require.Fail("test timed out")
-	}
-}
-
-// TestJailFetchRace tests multiple fetch callbacks,
-// supposed to be run with '-race' flag.
-func (s *CellTestSuite) TestJailFetchRace() {
-	body := `{"key": "value"}`
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		w.Write([]byte(body))
-	}))
-	defer server.Close()
-	require := s.Require()
-
-	cell, err := s.jail.NewCell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-	defer cell.Stop()
-
-	dataCh := make(chan otto.Value, 1)
-	errCh := make(chan otto.Value, 1)
-
-	err = cell.Set("__captureSuccess", func(res otto.Value) { dataCh <- res })
-	require.NoError(err)
-	err = cell.Set("__captureError", func(res otto.Value) { errCh <- res })
-	require.NoError(err)
-
-	// run JS code for fetching valid URL
-	_, err = cell.Run(`fetch('` + server.URL + `').then(function(r) {
-		return r.text()
-	}).then(function(data) {
-		__captureSuccess(data)
-	}).catch(function (e) {
-		__captureError(e)
-	})`)
-	require.NoError(err)
-
-	// run JS code for fetching invalid URL
-	_, err = cell.Run(`fetch('http://👽/nonexistent').then(function(r) {
-		return r.text()
-	}).then(function(data) {
-		__captureSuccess(data)
-	}).catch(function (e) {
-		__captureError(e)
-	})`)
-	require.NoError(err)
-
-	for i := 0; i < 2; i++ {
-		select {
-		case data := <-dataCh:
-			require.True(data.IsString())
-			require.Equal(body, data.String())
-		case e := <-errCh:
-			require.True(e.IsObject())
-			name, err := e.Object().Get("name")
-			require.NoError(err)
-			require.Equal("Error", name.String())
-			_, err = e.Object().Get("message")
-			require.NoError(err)
-		case <-time.After(1 * time.Second):
-			require.Fail("test timed out")
-			return
-		}
-	}
-}
-
-// TestJailLoopCancel tests that cell.Stop() really cancels event
-// loop and pending tasks.
-func (s *CellTestSuite) TestJailLoopCancel() {
-	require := s.Require()
-
-	// load Status JS and add test command to it
-	s.jail.BaseJS(baseStatusJSCode)
-	s.jail.Parse(testChatID, ``)
-
-	cell, err := s.jail.Cell(testChatID)
-	require.NoError(err)
-	require.NotNil(cell)
-
-	var count int
-	err = cell.Set("__captureResponse", func(val string) otto.Value {
-		count++
-		return otto.UndefinedValue()
-	})
-	require.NoError(err)
-
-	_, err = cell.Run(`
-		function callRunner(val, delay){
-			return setTimeout(function(){
-				__captureResponse(val);
-			}, delay);
-		}
-	`)
-	require.NoError(err)
-
-	// Run 5 timeout tasks to be executed in: 1, 2, 3, 4 and 5 secs
-	for i := 1; i <= 5; i++ {
-		_, err = cell.Call("callRunner", nil, "value", i*1000)
-		require.NoError(err)
-	}
-
-	// Wait 1.5 second (so only one task executed) so far
-	// and stop the cell (event loop should die)
-	time.Sleep(1500 * time.Millisecond)
-	cell.Stop()
-
-	// check that only 1 task has increased counter
-	require.Equal(1, count)
-
-	// wait 2 seconds more (so at least two more tasks would
-	// have been executed if event loop is still running)
-	<-time.After(2 * time.Second)
-
-	// check that counter hasn't increased
-	require.Equal(1, count)
 }
