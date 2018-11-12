18948466b36ce58bf3bad9f567a024820c928854
Andrea Franz <andrea@gravityblast.com>
2018-11-12 18:36:20 +0100

    debug CI

--- _assets/scripts/install_android_ndk.sh
+++ _assets/scripts/install_android_ndk.sh
@@ -1,7 +1,13 @@
-#!/bin/bash		
-		
-curl https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip -o android-ndk-r17b.zip		
-unzip -q android-ndk-r17b.zip && rm android-ndk-r17b.zip		
-mv android-ndk-r17b $HOME		
+#!/bin/bash
+
+echo "downloading"
+pwd
+ls
+curl https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip -o android-ndk-r17b.zip
+echo "unzipping"
+rm -rf android-ndk-r17
+unzip -q android-ndk-r17b.zip && rm android-ndk-r17b.zip
+echo "moving"
+mv android-ndk-r17b $HOME
 export ANDROID_NDK=$HOME/android-ndk-r17b
 export ANDROID_HOME=$HOME/android-ndk-r17b
