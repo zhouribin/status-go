e94d1884eae8fca24d09165799101c709ca700d5
Andrea Franz <andrea@gravityblast.com>
2018-11-12 18:53:11 +0100

    debug CI

--- _assets/scripts/install_android_ndk.sh
+++ _assets/scripts/install_android_ndk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 curl https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip -o android-ndk-r17b.zip
-rm -rf $HOME/android-ndk-r17
-unzip -q android-ndk-r17b.zip -d $HOME && rm android-ndk-r17b.zip
+ls /home/jenkins/android-ndk-r17b/platforms
+rm -rf $HOME/android-ndk-r17b
+unzip -q android-ndk-r17b.zip && rm android-ndk-r17b.zip
+mv android-ndk-r17b $HOME
 export ANDROID_NDK=$HOME/android-ndk-r17b
 export ANDROID_HOME=$HOME/android-ndk-r17b
